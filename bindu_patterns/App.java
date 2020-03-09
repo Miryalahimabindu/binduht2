3a56bd679c0d680fff86b0ac59e2b90a7807f6bb
Miryalahimabindu <61867613+Miryalahimabindu@users.noreply.github.com>
2020-03-09 21:36:57 +0530

    Add files via upload

---
+++ bindu_patterns/App.java
@@ -0,0 +1,13 @@
+package com.bindu.bindu_patterns;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
